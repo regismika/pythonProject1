6ea7846cb60f6112fdbb5f774f3711371f22e6b5
regismika <regimantas.mikaliunas@gmail.com>
2024-09-23 20:31:42 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,151 @@
+from xml.dom.minidom import ProcessingInstruction
+
+print (15)
+print (12)
+print (89)
+print (5.8)
+print (78 * 2)
+print(5 + 3)
+print(7 / 2)
+print(85 - 32)
+print(round(5.2 * 3, 2))
+print(round(9.4 * 0.5, 2))
+print(4.2 / 2)
+print(7 + 2 * 3)
+print((7 + 2) * 3)
+print(52 + 74 + 32)
+print(87 - 65 + 14)
+print(round(79 / (5 - 2), 2))
+print(2 ** 4)
+print(8 ** 3)
+print(round(14 ** 0.5, 2))
+print(2 % 2)
+print(3 % 2)
+print(11 % 2)
+print(13 % 2)
+print(10 % 2)
+print(15 % 2)
+print(15 % 3)
+print(15 % 4)
+print(15 % 5)
+print(15 % 6)
+print(15 % 7)
+print(15 % 8)
+print(15 % 9)
+print(5 // 2)
+print(6 // 3)
+print(6 // 4)
+print(80 // 3)
+print(45 // 4)
+print(45 // 3)
+print(type(6))
+print(type (2.5))
+print(type (157))
+print(type(-8.2))
+print(type(2 / 5))
+print(type(8 + 9))
+print(type(8.5 + 3))
+
+vardas = "Regimantas"
+pavarde = "Mikaliūnas"
+amzius = "45"
+ugis = "172"
+svoris = "88"
+aukstojiMokykla = "MRU"
+akademinesGrupeKodas = "DA"
+kursas = "duomenu_analitikas"
+studijuProgramosPavadinimas = "TA"
+atsiskaitytuKredituSkaicius = "99"
+print("vardas -", vardas)
+print("pavarde -", pavarde)
+print("amzius -", amzius)
+print("ugis -", ugis)
+print("svoris -", svoris)
+print("aukstojiMokykla -", aukstojiMokykla)
+print("akademinesGrupeKodas -", akademinesGrupeKodas)
+print("kursas -", kursas)
+print("studijuProgramosPavadinimas -", studijuProgramosPavadinimas)
+print("atsiskaitytuKredituSkaicius -", atsiskaitytuKredituSkaicius)
+
+pavadinimas = "radviliskis"
+valstybe = "lietuva"
+apskritis = "siauliu"
+ikurimo_data = "1567_m"
+meras = "kazimieras"
+plotas_kv_km = "1635_m²"
+gyventoju_skaicius = "14976"
+kursu_tikslas = "naujos_zinios"
+print("pavadinimas -", pavadinimas)
+print("valstybe -", valstybe)
+print("apskritis -", apskritis)
+print("ikurimo_data -", ikurimo_data)
+print("meras -", meras)
+print("plotas_kv_km -", plotas_kv_km)
+print("gyventoju_skaicius -", gyventoju_skaicius)
+
+print(f"mano vardas {vardas}, o pavardė {pavarde}, \n kursu_tikslas {kursu_tikslas}")
+print("vardas", vardas, "pavardė", pavarde, "\n", "naujos_zinios")
+
+# 2
+sk1 = 15
+sk2 = 3
+print(f"{sk1} + {sk2} = {sk1 + sk2}")
+# 3
+sk3 = 9
+print(f"{sk1} + {sk2} + {sk3} = {sk1 + sk2 + sk3}")
+print(f"{sk1} - {sk2} - {sk3} = {sk1 - sk2 - sk3}")
+print(f"{sk1} * {sk2} * {sk3} = {sk1 * sk2 * sk3}")
+print(f"({sk1} / {sk2}) * {sk3} = {sk1 / sk2 / sk3}")
+# 4
+print(sk1 * 5)
+print(sk2 * 2)
+# 5 Paprašykite vartotojo įvesti savo vardą, amžių ir kodėl pasirinko
+# programavimą. Įvestą informaciją išveskite kaip nors tvarkingai, sakiniu ar
+# atskirose eilutėse su prierašais
+# print("kuo tu vardu?")
+# vardas = input ()
+# print(f"labas {vardas}")
+# print("kiek tau metu")
+# metai = input()
+# print(f"sveiks seni {metai}-iu metu")
+# 6
+# print("iveskite koki nors skaiciu prasau")
+# skaicius = int(input())
+# print("ivesta:", skaicius, type(skaicius))
+# 7
+# print("iveskite koki nors skaiciu prasau")
+# skaicius = input()
+# print(f"{skaicius} \n{skaicius * 2} \n{skaicius * 3}")
+# 8 Paprašykite vartotojo įvesti vardą, amžių, ūgį (metais) (nepamirškite kąreikia iškonvertuoti
+# iš str į int ar float). Išveskite gautus duomenis ir jų tipus
+# print("vardas:")
+# vardas = input()
+# print("amzius:")
+# amzius = int(input())
+# print("ugis:")
+# ugis = int(input())
+# print("ivesta informacija:")
+# print(vardas, type(vardas))
+# print(amzius, type(amzius))
+# print(ugis, type(ugis))
+# 9 Leiskite vartotojui įvesti metrus. Išveskite kiek tai gaunasi centimetrais, milimetrais ir kilometrais
+
+sk1 = 5
+sk2 = 7
+sk3 = 8
+if sk1 == sk2:
+    print("sk1 lygu sk2")
+else:
+    print("sk1 nelygu sk2")
+
+git config --global user.name "regismika"
+git config --global user.email "regimantas.mikaliunas@gmail.com"
+
+
+
+
+
+
+
+
+
